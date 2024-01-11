f97b5ed243da3502efb2b7393cb47eee4231eb8f
Jbrockhoff <150721892+Jbrockhoff@users.noreply.github.com>
2024-01-11 09:35:43 -0600

    Added starter html and main questions/ansswers in JS

---
+++ assets/settings.js
@@ -0,0 +1,47 @@
+const questions = [
+    {
+        question:  "Commonly used data types DO NOT include the following: ",
+        answerA: "strings",
+        answerB: "booleans",
+        answerC: "alerts",
+        answerD: "numbers",
+        correctanswer: "answerC"
+    },
+
+    {
+        question:  "The condition if/else statement is enclosed within:  ",
+        answerA: "numbers and strings",
+        answerB: "curly braces",
+        answerC: "parenthesis",
+        answerD: "square brackets",
+        correctanswer: "answerB"
+    },
+
+    {
+        question:  "Arrays in JavaScript can be used to store: ",
+        answerA: "numbers and strings",
+        answerB: "other arrays",
+        answerC: "booleans",
+        answerD: "all of the above",
+        correctanswer: "answerD"
+    },
+
+    {
+        question:  "String valuesmust be enclosed within ________ when assigned to variables: ",
+        answerA: "commas",
+        answerB: "curly braces",
+        answerC: "quotes",
+        answerD: "parenthesis",
+        correctanswer: "answerC"
+    },
+    {
+        question:  "A very useful tool in debugging for printing content to the debugger is: ",
+        answerA: "JavaScript",
+        answerB: "Terminal/Bash",
+        answerC: "for loops",
+        answerD: "console.log",
+        correctanswer: "answerD"
+    },
+
+
+]
